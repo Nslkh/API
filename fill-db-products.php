e3945713fb2e0fdcff3ed9b8c8d8c59f5fe7f469
Nslkh <olluzran@gmail.com>
2021-03-25 19:52:45 +0500

    add products based on category

---
+++ fill-db-products.php
@@ -0,0 +1,17 @@
+<?php
+$categories = json_decode(file_get_contents('mock.json'), true);
+
+$connection = mysqli_connect($host = 'localhost',  $user = 'root', $password = '',$database =   'cloth_shop_api',  $port = '3306',);
+
+foreach ($categories as $category_id => $category){
+
+  foreach ($category['items'] as $product){
+      $query = "INSERT INTO products(name, img, price, category_id) VALUES ('{$product['name']}', '{$product['img']}', '{$product['price']}', '{$category_id}')";
+      $result = mysqli_query($connection, $query);
+
+      if ($result) {
+          echo $product['name'] . " was added successfully to " . $category['title'] . "<br>";
+      }
+  }
+
+}
