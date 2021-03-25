9970e6f5974205d55c2b6c21ccb8cf731e26567c
Nslkh <olluzran@gmail.com>
2021-03-25 18:37:23 +0500

    add category to db

--- fill-db-categories.php
+++ fill-db-categories.php
@@ -1,4 +1,12 @@
 <?php
-$data = json_decode(file_get_contents('mock.json'), true);
+$categories = json_decode(file_get_contents('mock.json'), true);
 
 $connection = mysqli_connect($host = 'localhost',  $user = 'root', $password = '',$database =   'cloth_shop_api',  $port = '3306',);
+
+foreach($categories as $category){
+  $query = "INSERT INTO categories(title, image) VALUES ('{$category['title']}','{$category['image']}')";
+  $result = mysqli_query($connection,$query);
+  if($result){
+    echo $category['title'] . " added successfully<br>";
+  }
+}
