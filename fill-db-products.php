444bfa16a938a453e2a6f9668cd961e87c76d5a8
Nslkh <olluzran@gmail.com>
2021-04-03 21:42:31 +0500

    bug fixed

--- fill-db-products.php
+++ fill-db-products.php
@@ -5,17 +5,15 @@ $db_config = $config['mysql'];
 
 $categories = json_decode(file_get_contents('mock.json'), true);
 
-$connection = mysqli_connect($db_config['host'],$db_config['user'],$db_config['password'], $db_config['database'],$db_config['port']);
+$connection = mysqli_connect($db_config['host'], $db_config['user'], $db_config['password'], $db_config['database'], $db_config['port']);
 
-foreach ($categories as $category_id => $category){
+foreach ($categories as $category_id => $category) {
+    foreach ($category['items'] as $product) {
+        $query = "INSERT INTO products(name, img, price, category_id) VALUES ('{$product['name']}', '{$product['img']}', '{$product['price']}', '{$category['id']}')";
+        $result = mysqli_query($connection, $query);
 
-  foreach ($category['items'] as $product){
-      $query = "INSERT INTO products(name, img, price, category_id) VALUES ('{$product['name']}', '{$product['img']}', '{$product['price']}', '{$category_id}')";
-      $result = mysqli_query($connection, $query);
-
-      if ($result) {
-          echo $product['name'] . " was added successfully to " . $category['title'] . "<br>";
-      }
-  }
-
+        if ($result) {
+            echo $product['name'] . " was added successfully to " . $category['title'] . "<br>";
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// TODO: Implement showing products regarding each category, hints are given
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
@@ -7,23 +8,26 @@ $config = require 'config.php';
 $db_config = $config['mysql'];
 
 $connection = mysqli_connect($db_config['host'],$db_config['user'],$db_config['password'], $db_config['database'],$db_config['port']);
-
-$sql = " SELECT * FROM categories";
+$sql = "SELECT * FROM categories";
 $query = mysqli_query($connection, $sql);
 $data = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
+//$data = json_decode(file_get_contents('mock.json'), true);
 
-// $data = json_decode(file_get_contents('mock.json'), true); 
+//echo "<pre>";
+//var_dump($data);
+//echo "</pre>";
 
-  // echo "<pre>";
-  // var_dump($data);
-  // echo "</pre>";
+$result = [];
 
+foreach ($data as $category_id => $category){
+    $sql = "SELECT * FROM products WHERE category_id='" . $category['id'] . "'";
 
-$result = [];  
-foreach($data as $category_id => $category){
-  $category['items'] = $category;
-  $result[] = $category; 
+    $query = mysqli_query($connection, $sql);
+    $data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+    $category['items'] = $data;
+    $result[] = $category;
 }
 
-echo json_encode($result, JSON_PRETTY_PRINT);
+echo json_encode($result, JSON_PRETTY_PRINT );
